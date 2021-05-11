84f087b925d887bfd110b171cedf58c484ab6a93
athena.spaceboy <athena.spaceboy21@gmail.com>
2021-05-11 23:52:02 +0900

    initial uploading

---
+++ src/views/examples/Lottery.js
@@ -0,0 +1,97 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// reactstrap components
+import {
+  Button,
+  Row,
+  Col,
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import Footer from "components/Footer/Footer.js";
+import "../../assets/css/star.css"
+import "../../assets/css/lottery.css"
+
+export default function Lottery() {
+  React.useEffect(() => {
+    document.body.classList.toggle("landing-page");
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      document.body.classList.toggle("landing-page");
+    };
+  },[]);
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="wrapper starsContainer">
+        <div className="starsContainer">
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+        </div>
+
+        <div className="page-header">
+          <img
+            alt="..."
+            className="path"
+            src={require("assets/img/blob.png").default}
+          />
+          <img
+            alt="..."
+            className="path2"
+            src={require("assets/img/path2.png").default}
+          />
+          <img
+            alt="..."
+            className="shapes triangle"
+            src={require("assets/img/triunghiuri.png").default}
+          />
+          <img
+            alt="..."
+            className="shapes wave"
+            src={require("assets/img/waves.png").default}
+          />
+          <img
+            alt="..."
+            className="shapes squares"
+            src={require("assets/img/patrat.png").default}
+          />
+          <img
+            alt="..."
+            className="shapes circle"
+            src={require("assets/img/cercuri.png").default}
+          />
+          <div className="content-center">
+            <Row className="row-grid justify-content-between align-items-center text-left">
+              <Col lg="12" md="12">
+                  <div  className="lottery-content">
+                    <div  className="lottery-left-pannel">
+                    </div>
+                  </div>
+                  
+              </Col>
+            </Row>
+          </div>
+        </div>
+       <Footer />
+      </div>
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor : ['active'],
+      width: {
+        'w-535': '535px',
+        'w-834': '834px'
+      }
+    },
+  },
+  plugins: [],
+}
